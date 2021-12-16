7f06ee53720bf152ffce707dc5a1c2e069c396c4
Vasanth Sarathy <vsarathy@sift.net>
2021-12-16 17:04:31 -0500

    Adding pddlgym connection

--- agents/random/randomagent.py
+++ agents/random/randomagent.py
@@ -1,4 +1,5 @@
 from agents.agent import Agent
+import pddlgym
 
 class RandomAgent(Agent):
     """
@@ -8,9 +9,23 @@ class RandomAgent(Agent):
     def __init__(self, ctx):
         super().__init__()
         self.config = ctx
-        self.env = self.config['env']
+        self.env = pddlgym.make(self.config['env'])
+        self.goal = self.config['goal']
         print("🎲 Random agent initialized.")
 
     def _solve(self):
         print("Solving...")
-        return {'plan':[], "status":"fail"}
+        obs, _ = self.env.reset()
+        print(f"Initial State:\n {obs}")
+        print("-------------")
+        actions = []
+        for _ in range(10):
+            action = self.env.action_space.sample(obs)
+            print(action)
+            actions.append(action)
+            obs, _, _, _ = self.env.step(action)
+        print("-------------")
+        print(f"Final State: \n {obs}")
+        return {'plan':actions, "status":"completed"}
+
+
